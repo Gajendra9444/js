a7e9965bd441fa30019c16f9081bab982978c27a
Gajendra9444 <118856482+Gajendra9444@users.noreply.github.com>
2025-01-11 15:17:31 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version //
+
+//alert(3+3)// we are using nodeJS, not browser 
+
+console.log( 3 + 3 )
+
+console.log("gajendra")\
+
+
+let name = "gajendra"
+let age = 22
+let isLoggedIn = false 
+
