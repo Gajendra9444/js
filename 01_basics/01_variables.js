b9a51df3216e9f191691a52eb0a3103dac958381
Gajendra9444 <118856482+Gajendra9444@users.noreply.github.com>
2025-01-11 14:45:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12433
+let accountEmail = "abc@.com"
+var accountPassword = "972763"
+accountCity = "dhangadhi"
+
+
+accountEmail = "dad@gmail.com"
+accountPassword = "2345"
+accountCity = "mahendranagar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
